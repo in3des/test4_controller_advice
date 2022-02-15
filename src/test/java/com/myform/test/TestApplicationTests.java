016284a73afa7543e6d81616e278db86c66c2f0e
in3des <weabs@mail.ru>
2022-02-15 20:52:27 +0400

    cleanup compact

--- src/test/java/com/myform/test/TestApplicationTests.java
+++ src/test/java/com/myform/test/TestApplicationTests.java
@@ -1,20 +1,16 @@
 package com.myform.test;
 
-//import com.myform.test.configuration.TestProfileJPAConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @AutoConfigureMockMvc
 @Sql(scripts = "/scripts/cleanup_data.sql",
     executionPhase = BEFORE_TEST_METHOD)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-//@ContextConfiguration(initializers = {TestProfileJPAConfig.class})
+@SpringBootTest
 class TestApplicationTests{
 
     @Test
