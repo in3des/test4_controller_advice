121fb7a6017ad32337228f62766bce4a530aa888
in3des <weabs@mail.ru>
2022-02-15 22:53:33 +0400

    cleanup all

--- src/test/java/com/myform/test/TestApplicationTests.java
+++ src/test/java/com/myform/test/TestApplicationTests.java
@@ -1,10 +1,8 @@
 package com.myform.test;
 
-import com.myform.test.configuration.TestProfileJPAConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
@@ -13,7 +11,6 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TE
 @Sql(scripts = "/scripts/cleanup_data.sql",
     executionPhase = BEFORE_TEST_METHOD)
 @SpringBootTest
-@ContextConfiguration(initializers = {TestProfileJPAConfig.class})
 class TestApplicationTests{
 
     @Test
